b6c67096ac5b554d469ebcda258a82185a60eff1
Oliver Hinckel <info@ollisnet.de>
2022-09-29 21:21:57 +0200

    Add methods to remove and get jobs from scheduler

--- framarama/jobs.py
+++ framarama/jobs.py
@@ -15,11 +15,16 @@ class Scheduler:
     def add(self, func, *args, **kwargs):
         self._scheduler.add_job(lambda: func(), *args, **kwargs)
 
+    def remove(self, name):
+        self._scheduler.remove_job(name)
+
+    def get(self, name):
+        return self._scheduler.get_job(name)
+
     def setup(self):
         self._scheduler.start()
 
         for module in [config_jobs, api_jobs, frontend_jobs]:
             if (hasattr(module, 'Jobs')):
                 _jobs = module.Jobs(self)
-
 
