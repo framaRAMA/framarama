07cbdfff771059d26b29698b8c3757c324d37b07
Oliver Hinckel <info@ollisnet.de>
2022-09-29 21:23:02 +0200

    Try to only initialize scheduler when running as application
    Avoid starting scheduler when running other commands like makemigrations or
    migrate.

--- frontend/apps.py
+++ frontend/apps.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.apps import AppConfig
 
 
@@ -6,7 +8,12 @@ class FrontendConfig(AppConfig):
     name = 'frontend'
 
     def ready(self):
-        from framarama.jobs import Scheduler
-        scheduler = Scheduler()
-        scheduler.setup()
+        is_manage_py = any(arg.casefold().endswith("manage.py") for arg in sys.argv)
+        is_runserver = any(arg.casefold() == "runserver" for arg in sys.argv)
+
+        # Instance is running application (no service tasks or other commands)
+        if (is_manage_py and is_runserver) or (not is_manage_py):
+            from framarama.jobs import Scheduler
+            scheduler = Scheduler()
+            scheduler.setup()
 
