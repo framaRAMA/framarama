e567fad448d7b4af17830063d80070ab4fb264f6
Oliver Hinckel <info@ollisnet.de>
2022-12-11 18:55:35 +0100

    Start to add files for documentation purposes

---
+++ docs/systemd/start.sh
@@ -0,0 +1,20 @@
+
+# Change to application dir, activate venv and start application
+cd "$(dirname $(realpath $0))"
+
+# Start window manager
+openbox-session &
+
+. ./venv/bin/activate
+while [ "1" = "1" ] ; do
+
+  # Set startup background
+  feh --bg-scale ./common/static/common/background.png
+
+  # Start application
+  gunicorn --capture-output -b 0.0.0.0:8000 framarama.wsgi
+
+  sleep 5
+
+done
+
