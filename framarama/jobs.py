66ecb3267578a53a1124e2b493a8be5ac360e540
Oliver Hinckel <info@ollisnet.de>
2022-12-30 00:13:08 +0100

    Change Scheduler.add() to accept func_(args|kwargs) for callback function

--- framarama/jobs.py
+++ framarama/jobs.py
@@ -13,7 +13,9 @@ class Scheduler:
         return self._scheduler.get_jobs()
 
     def add(self, func, *args, **kwargs):
-        self._scheduler.add_job(lambda: func(), *args, **kwargs)
+        _func_args = kwargs.pop('func_args', {})
+        _func_kwargs = kwargs.pop('func_kwargs', {})
+        self._scheduler.add_job(lambda *args, **kwargs: func(*args, **kwargs), args=_func_args, kwargs=_func_kwargs, *args, **kwargs)
 
     def remove(self, name):
         self._scheduler.remove_job(name)
