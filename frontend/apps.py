70f173b6f3395462a0db83dae01d04751faeefe9
Oliver Hinckel <info@ollisnet.de>
2022-12-30 00:18:15 +0100

    Hold scheduler instance in private field of FrontendConfig and add get_scheduler() method

--- frontend/apps.py
+++ frontend/apps.py
@@ -7,6 +7,9 @@ class FrontendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'frontend'
 
+    def get_scheduler(self):
+        return self._scheduler
+
     def ready(self):
         is_manage_py = any(arg.casefold().endswith("manage.py") for arg in sys.argv)
         is_runserver = any(arg.casefold() == "runserver" for arg in sys.argv)
@@ -14,6 +17,6 @@ class FrontendConfig(AppConfig):
         # Instance is running application (no service tasks or other commands)
         if (is_manage_py and is_runserver) or (not is_manage_py):
             from framarama.jobs import Scheduler
-            scheduler = Scheduler()
-            scheduler.setup()
+            self._scheduler = Scheduler()
+            self._scheduler.setup()
 
