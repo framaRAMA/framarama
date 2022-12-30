96b7126e2d47b593d9e1416439cf99abaa8e661b
Oliver Hinckel <info@ollisnet.de>
2022-12-30 12:09:37 +0100

    Keep track of running jobs and generate dynamic ID in Scheduler.trigger()
    Using event listeners to keep track of running jobs in a list. When using trigger()
    method to start a job generate a dynamic job ID with current ID and timestamp
    appended.
    
    The method running() can be used to check if a given job (by ID) is currently running
    by the scheduler executor. The optional flag starts_with can be used to check for
    jobs starting with given name (useful for job started with the trigger() method).

--- framarama/jobs.py
+++ framarama/jobs.py
@@ -1,4 +1,8 @@
+import time
+
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.events import EVENT_JOB_SUBMITTED, EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
+
 from config import jobs as config_jobs
 from api import jobs as api_jobs
 from frontend import jobs as frontend_jobs
@@ -8,6 +12,26 @@ class Scheduler:
 
     def __init__(self):
         self._scheduler = BackgroundScheduler()
+        self._scheduler.add_listener(self._event_start, EVENT_JOB_SUBMITTED)
+        self._scheduler.add_listener(self._event_success, EVENT_JOB_EXECUTED)
+        self._scheduler.add_listener(self._event_error, EVENT_JOB_ERROR)
+        self._running = []
+
+    def _event_start(self, event):
+        self._running.append(event.job_id)
+
+    def _event_success(self, event):
+        self._running.remove(event.job_id)
+
+    def _event_error(self, event):
+        self._running.remove(event.job_id)
+
+    def running(self, name, starts_with=False):
+        if starts_with:
+            _compare = lambda job: job.startswith(name)
+        else:
+            _compare = lambda job: job == name
+        return len([_job for _job in self._running if _compare(_job)]) > 0
 
     def jobs(self):
         return self._scheduler.get_jobs()
@@ -22,7 +46,9 @@ class Scheduler:
 
     def trigger(self, name, *func_args, **func_kwargs):
         _job = self.get(name)
-        self._scheduler.add_job(_job.func, args=func_args, kwargs=func_kwargs)
+        _job_id = _job.id + '_' + str(time.time())
+        self._scheduler.add_job(_job.func, id=_job_id, args=func_args, kwargs=func_kwargs)
+        return _job_id
 
     def get(self, name):
         return self._scheduler.get_job(name)
