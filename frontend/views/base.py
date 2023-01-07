d1875e0c5e4818d3d3f389bc2cd76abf57c5aa61
Oliver Hinckel <info@ollisnet.de>
2023-01-07 20:28:14 +0100

    Remove code duplication in BaseSetupView

--- frontend/views/base.py
+++ frontend/views/base.py
@@ -11,7 +11,7 @@ from framarama.base.views import BaseView
 class BaseSetupView(BaseView):
     PAGE_FE_STARTUP = 'fe_startup'
 
-    def _get(self, request, *args, **kwargs):
+    def _process(self, request, *args, **kwargs):
         _context = super()._get(request, *args, **kwargs)
         _frontend = frontend.Frontend.get()
         _config = _frontend.get_config()
@@ -22,16 +22,11 @@ class BaseSetupView(BaseView):
             self.redirect_startup(_context, url=self.url(request))
         return _context
 
+    def _get(self, request, *args, **kwargs):
+        return self._process(request, *args, **kwargs)
+
     def _post(self, request, *args, **kwargs):
-        _context = super()._post(request, *args, **kwargs)
-        _frontend = frontend.Frontend.get()
-        _config = _frontend.get_config()
-        _context['now'] = timezone.now()
-        _context['frontend'] = _frontend
-        _context['config'] = _config.get_config() if _frontend.is_configured() else None
-        if not _frontend.is_initialized() and self.view_name(request) != BaseSetupView.PAGE_FE_STARTUP:
-            self.redirect_startup(_context, url=self.url(request))
-        return _context
+        return self._process(request, *args, **kwargs)
 
     def redirect_startup(self, context, page=None, url=None):
         _query = 'startup=1'
