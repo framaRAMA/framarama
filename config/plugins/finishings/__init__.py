7f5957c3515be46d90ec7912f4e6cbf29bebcb7f
Oliver Hinckel <info@ollisnet.de>
2023-01-24 19:19:14 +0100

    Extend Model in predefined/base model classes for finishings

--- config/plugins/finishings/__init__.py
+++ config/plugins/finishings/__init__.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 
-class ColorStroke:
+class ColorStroke(models.Model):
     color_stroke = models.CharField(
         max_length=16,
         verbose_name='Foreground color', help_text='The foreground color (lines, text) to use in HEX (RGB)')
@@ -10,20 +10,32 @@ class ColorStroke:
         blank=True, null=True,
         verbose_name='Line width', help_text='The width to use when drawing lines')
 
+    class Meta:
+        abstract = True
 
-class ColorFill:
+
+class ColorFill(models.Model):
     color_fill = models.CharField(
         max_length=16, blank=True, null=True,
         verbose_name='Background color', help_text='The background color (fill) to use in HEX (RGB)')
 
+    class Meta:
+        abstract = True
 
-class ColorAlpha:
+
+
+class ColorAlpha(models.Model):
     color_alpha = models.IntegerField(
         blank=True, null=True,
         verbose_name='Transparency', help_text='The alpha value between 0 (transparent) and 100 (no transparency)')
 
+    class Meta:
+        abstract = True
+
 
 class ColorStrokeFillAlpha(ColorStroke, ColorFill, ColorAlpha):
-    pass
+
+    class Meta:
+        abstract = True
 
 
