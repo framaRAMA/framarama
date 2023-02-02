cb10fab9d84d04ce88f372e584c4e8ce1955da49
Oliver Hinckel <info@ollisnet.de>
2023-02-02 17:36:50 +0100

    Fix view name in config API urls for finishings

--- api/urls/config.py
+++ api/urls/config.py
@@ -13,7 +13,7 @@ router.register('frames/(?P<frame_id>[0-9]+)/items', views.FrameItemViewSet, 'fr
 router.register('displays', views.DisplayViewSet, 'display')
 router.register('displays/(?P<display_id>[0-9]+)/items/all', views.ItemDisplayViewSet, 'display_item_all')
 router.register('displays/(?P<display_id>[0-9]+)/items/next', views.NextItemDisplayViewSet, 'display_item_next')
-router.register('displays/(?P<display_id>[0-9]+)/finishings', views.FinishingDisplayViewSet, 'display_item_next')
+router.register('displays/(?P<display_id>[0-9]+)/finishings', views.FinishingDisplayViewSet, 'display_finishing')
 router.register('displays/(?P<display_id>[0-9]+)/status', views.StatusDisplayViewSet, 'display_status')
 
 urlpatterns = [
