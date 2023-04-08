b87df573bb72aadea71778a856e04a7bb0a1313f
Oliver Hinckel <info@ollisnet.de>
2023-04-08 15:23:52 +0200

    Fallback to default config and ask for option values not given on commandline

--- common/management/commands/setup.py
+++ common/management/commands/setup.py
@@ -2,7 +2,7 @@
 from django.core.management.base import BaseCommand
 
 from framarama.base import utils
-from config import models
+from frontend import models
 
 class Command(BaseCommand):
     help = 'Create or change initial system setup'
@@ -12,9 +12,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         _config = utils.Config.get().get_config()
+        _config = _config if _config else models.Config()
         _fields = sorted(vars(_config))
         self._dump('Current values:', _config, _fields)
-        _values = [_opt.split('=', 1) for _opt in options['set']] if options['set'] else []
+        _values = options['set'] if options['set'] else []
+        _values = [_opt.split('=', 1) if '=' in _opt else (_opt, None) for _opt in _values]
         _wrong = [_n for _n, _v in _values if _n not in _fields]
         if len(_wrong):
             self.stderr.write("Wrong values specified: {}".format(_wrong))
@@ -22,6 +24,8 @@ class Command(BaseCommand):
         if len(_values) == 0:
             return
         for _name, _value in _values:
+            if _value is None:
+                _value = input('Value for {}: '.format(_name))
             setattr(_config, _name, _value)
         self._dump('Updated values:', _config, [_n for _n, _v in _values])
         _config.save()
