54c5711de5995abba4131f2e9564266447041b36
Oliver Hinckel <info@ollisnet.de>
2023-06-09 21:02:34 +0200

    Refactor implementation of data_check command to use utils.Lists.process()

--- config/management/commands/data_check.py
+++ config/management/commands/data_check.py
@@ -23,23 +23,28 @@ class Command(BaseCommand):
             _processes.append([
                 'Database',
                 self._db_items,
-                lambda items: [_item for _item in items if _item.data_file and utils.Filesystem.file_exists(_item.data_file.path)],
+                lambda ids: [(_id, _id) for _id in ids if utils.Filesystem.file_exists(_id)],
                 lambda item: item.delete()
             ])
         if _fs or not _db:
             _processes.append([
                 'Filesystem',
                 self._fs_items,
-                lambda items: [_item.data_file.name for _item in models.Data.objects.filter(data_file__in=items)],
+                lambda ids: [(_item.data_file.name, _item) for _item in models.Data.objects.filter(data_file__in=ids)],
                 lambda item: utils.Filesystem.file_delete(item)
             ])
         for _name, _generator, _check, _delete in _processes:
-            _status = self._check_items(_name, _generator, _rm, _chunk, _check, _delete)
-            self._report_status(_name, _status, _rm)
+            self.stdout.write('{}: Checking items'.format(_name))
+            _stats = utils.Lists.process(
+                source=_generator(),
+                target_match=_check,
+                size=_chunk,
+                delete_func=lambda id, tval: print("delete {} {}".format(id, tval)))
+            self._report_status(_name, _stats, _rm)
 
     def _db_items(self):
         for _item in models.Data.objects.all():
-            yield _item
+            yield _item.data_file.name, _item
 
     def _fs_items(self):
         _skip = [models.Data, models.BaseImageData]
@@ -50,45 +55,14 @@ class Command(BaseCommand):
             _dirs = utils.Filesystem.file_match(_path, '.*', files=False, dirs=True, recurse=1)
             for _dir in [_dir[0] for _dir in _dirs if '/' in _dir[0]]:
                 for _item in utils.Filesystem.file_match(_path + '/' + _dir, '.*'):
-                    yield _path + '/' + _dir + '/' + _item[0]
-
-    def _check_items(self, prefix, items, remove, size, check_items, delete_item):
-        self.stdout.write('{}: Checking items'.format(prefix))
-        _stats = {'total': 0, 'ok': 0, 'delete': 0}
-        for _items in self._chunked(items, size):
-            _existing = check_items(_items)
-            _deletes = set(_items) - set(_existing)
-            for _delete in _deletes:
-                self.stdout.write('{}: Missing {}'.format(prefix, _delete))
-                if remove:
-                    delete_item(_delete)
-                    self.stdout.write('{}: Deleted {}'.format(prefix, _delete))
-            _stats['total'] = _stats['total'] + len(_items)
-            _stats['ok'] = _stats['ok'] + len(_existing)
-            _stats['delete'] = _stats['delete'] + len(_deletes)
-            self.stdout.write('{}: Checked {} items (last {} items: {} ok, {} delete)'.format(
-                prefix,
-                _stats['total'],
-                len(_items),
-                len(_existing),
-                len(_deletes)))
-        return _stats
-
-    def _chunked(self, items, size):
-        _result = []
-        for _item in items():
-            _result.append(_item)
-            if len(_result) == size:
-                yield _result
-                _result[:] = []
-        else:
-            yield _result
+                    _filename = _path + '/' + _dir + '/' + _item[0]
+                    yield _filename, _filename
 
     def _report_status(self, prefix, stats, remove):
         self.stdout.write('{}: {}, checked {} items ({} ok, {} delete)'.format(
             prefix,
-            'All OK' if stats['total'] == stats['ok'] else 'Cleaned up' if remove else 'Cleanup required',
+            'All OK' if stats['total'] == stats['update'] else 'Cleaned up' if remove else 'Cleanup required',
             stats['total'],
-            stats['ok'],
+            stats['update'],
             stats['delete']))
 
