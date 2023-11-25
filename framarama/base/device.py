f17e815bfa4597bb1c44b993b7173808efc49f18
Oliver Hinckel <info@ollisnet.de>
2023-11-25 01:02:04 +0100

    Still use target rev in Capabilities.app_revision() when on branch

--- framarama/base/device.py
+++ framarama/base/device.py
@@ -445,7 +445,7 @@ class Capabilities:
             _update_revs = {}
             for _i, _rev in enumerate(_revisions):
                 if _rev == 'master':
-                    _logs_update = Capabilities._git_log(['-1', '{0}..{1}/{0}'.format(_logs[0]['hash'], _remote)])
+                    _logs_update = Capabilities._git_log(['-1', '{0}..{2}/{1}'.format(_logs[0]['hash'], _rev, _remote)])
                 elif _i == 0 or _logs_update:
                     _logs_update = Capabilities._git_log(['-1', '{0}..{1}'.format(_logs[0]['hash'], _rev)])
                 if _logs_update:
