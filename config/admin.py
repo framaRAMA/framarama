6c4af8f515786b5db7e5f8484634d6bd5e3f6355
Oliver Hinckel <info@ollisnet.de>
2023-12-02 13:23:28 +0100

    Provide more models in admin interface

--- config/admin.py
+++ config/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Frame
+from . import models
 
-admin.site.register(Frame)
+admin.site.register(models.Frame)
+admin.site.register(models.Source)
+admin.site.register(models.SourceStep)
+admin.site.register(models.Sorting)
+admin.site.register(models.Finishing)
+admin.site.register(models.FrameContext)
+admin.site.register(models.Item)
+admin.site.register(models.Display)
+admin.site.register(models.DisplayStatus)
+admin.site.register(models.DisplayItem)
 
