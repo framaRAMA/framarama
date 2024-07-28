ea5cc06e4047c7da0ad48a8c9d19813346f2c16e
Oliver Hinckel <info@ollisnet.de>
2024-07-28 19:52:37 +0200

    Make use of data path for integrated SQLite database

--- framarama/settings.py
+++ framarama/settings.py
@@ -124,7 +124,7 @@ LOGGING = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     },
 }
 DATABASE_ROUTERS = [
